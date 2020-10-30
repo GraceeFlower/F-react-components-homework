ceac90c7988c2bb5d6142fdc13c66efb06e464c3
GraceeFlower <1491644103@qq.com>
2020-10-30 17:04:35 +0800

    [Min] feat: finished

--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,47 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: '',
+    };
+  }
+
+  updateMsg = (event) => {
+    this.setState({
+      msg: event.target.value,
+    });
+  };
+
+  handlerClick = () => {
+    this.props.handleMessage(this.state.msg);
+    this.setState({
+      msg: '',
+    });
+  };
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.props.handleMessage(this.state.msg);
+      this.setState({
+        msg: '',
+      });
+    }
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input
+          type="text"
+          onChange={this.updateMsg}
+          value={this.state.msg}
+          onKeyPress={this.handleKeyPress}
+        />
+        <button type="button" className="btn-submit" onClick={this.handlerClick}>
+          Send
+        </button>
       </footer>
     );
   }
